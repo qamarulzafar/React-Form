3aa99238bf65c351dc0e7ab3c2eeb81d4d28b9a1
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2024-11-03 04:18:58 -0700

    heading change

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
             <div className="max-w-md mx-auto">
               <div>
                 <h1 className="text-2xl font-semibold">
-                  Login Form
+                  Login Form Made By Qamar
                 </h1>
               </div>
               <div className="divide-y divide-gray-200">
@@ -95,32 +95,3 @@ const App = () => {
 };
 
 export default App;
-
-// import { useForm } from "react-hook-form";
-
-// export default function App() {
-//   const {
-//     register,
-//     handleSubmit,
-//     watch,
-//     formState: { errors },
-//   } = useForm();
-//   const onSubmit = (data) => console.log(data);
-
-//   // console.log(("example")); // watch input value by passing the name of it
-
-//   return (
-//     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       {/* register your input into the hook by invoking the "register" function */}
-//       <input placeholder="Enter your eample" {...register("example")} />
-
-//       {/* include validation with required or other standard HTML validation rules */}
-//       <input {...register("exampleRequired", { required: true })} />
-//       {/* errors will return when field validation fails  */}
-//       {/* {errors.exampleRequired && <span>This field is required</span>} */}
-
-//       <input type="submit" />
-//     </form>
-//   );
-// }
